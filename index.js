68e0b5aa565304e3d0b58a6376e30ee35d93b9ed
NovDevCodes042 <143083634+NovDevCodes042@users.noreply.github.com>
2023-10-26 03:56:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const cors = require ('cors');
+// const PORT = 7000;
+const PORT = process.env.PORT || 7000;
+const goalRouter = require("./Routes/GoalRouter.js")
 
+//midware 
+app.use(express.json())
+app.use(cors());
+
+//routes
+app.use("/api/goals", goalRouter)
+
+
+//ds connection
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            dbName: "goals",
+        })
+        app.listen(PORT, () => {
+            console.log((`Server running on port  ${PORT}...`));
+        })
+    }catch (error) {
+        console.log(error);
+    }
+};
+startServer(); 
+
+
+///error routes
+app.use((req, res) => {
+    res.status(404).json({success: false, msg: "Resource not found"});
+});
